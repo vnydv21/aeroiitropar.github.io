2122273612e3e7f3b9215773eeaa3816432b6a62
KpskyVnzt <CoderVin@users.noreply.github.com>
2022-10-24 10:28:53 +0530

    Update

--- scripts/events.js
+++ scripts/events.js
@@ -1,3 +1,16 @@
-$('#calendar').evoCalendar({
-    theme: 'Royal Navy'
+$(document).ready(function() {
+    $('#calendar').evoCalendar({
+        theme: "Royal Navy",
+
+        calendarEvents: [
+            {
+              id: 'ISMP_1', // Event's ID (required)
+              name: "ISMP Meet", // Event name (required)
+              date: "October/29/2022", // Event date (required)
+              type: "event", // Event type (required)
+              description: "Welcome to the ISMP meet!",              
+              color: "#63d867" // Event custom color (optional)
+            },
+          ]
+    })
 })
