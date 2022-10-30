ee3dc30f5565e00de4180027b8cb7a4738371cc5
KpskyVnzt <CoderVin@users.noreply.github.com>
2022-10-30 22:14:51 +0530

    Update

--- scripts/events.js
+++ scripts/events.js
@@ -5,10 +5,42 @@ $(document).ready(function() {
         calendarEvents: [
             {
               id: 'ISMP_1',
-              name: "ISMP Meet",
-              date: "October/29/2022",
+              name: "Welcome Session",
+              date: "November/1/2022",
               type: "event",
-              description: "Welcome to the ISMP meet!",              
+              description: "ISMP - 2022 Btech - G1, G2 <br> 10 pm : M4",
+              color: "#63d867"
+            },
+            {
+              id: 'ISMP_2',
+              name: "Welcome Session",
+              date: "November/2/2022",
+              type: "event",
+              description: "ISMP - 2022 Btech - G3, G4 <br> 10 pm : M4",
+              color: "#63d867"
+            },
+            {
+              id: 'ISPM_3',
+              name: "Welcome Session",
+              date: "November/3/2022",
+              type: "event",
+              description: "ISMP - 2022 Btech - G5, G6 <br> 10 pm : M4",
+              color: "#63d867"
+            },
+            {
+              id: 'ISPM_4',
+              name: "Welcome Session",
+              date: "November/4/2022",
+              type: "event",
+              description: "ISMP - 2022 Btech - G7, G8 <br> 10 pm : M4",
+              color: "#63d867"
+            },
+            {
+              id: 'ISPM_5',
+              name: "Welcome Session",
+              date: "November/5/2022",
+              type: "event",
+              description: "ISMP - 2022 Btech - G9, G10 <br> 10 pm : M4",
               color: "#63d867"
             },
           ]
